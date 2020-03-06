6632b9ca49df9db8c49fd38b740a1a893593fbef
unknown <phamhongtien19@gmail.com>
2020-03-06 18:25:08 +0700

    add new file

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php 
+//cấu hình kết nối
+$conn = mysqli_connect('sql111.epizy.com','epiz_24116039','7qe7b4JYkMp','epiz_24116039_a') or die ('Không thể kết nối tới database');
+//xét kiểu ký tự
+mysqli_set_charset($conn,'utf8');
+
+?>
